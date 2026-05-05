007e5bb532ca208b3e3883b23124f03dd4ab9b4c
Anelis Gheorghiaș <98900827+Anelisgh@users.noreply.github.com>
2026-05-05 12:18:58 +0300

    Update 04_formele_de_transparenta.sql

--- 04_formele_de_transparenta.sql
+++ 04_formele_de_transparenta.sql
@@ -43,3 +43,36 @@ CREATE OR REPLACE SYNONYM AEROPORT FOR AEROPORT@link_bd_am;
 CREATE OR REPLACE SYNONYM AVION FOR AVION@link_bd_am;
 CREATE OR REPLACE SYNONYM ZBOR FOR ZBOR@link_bd_am;
 CREATE OR REPLACE SYNONYM PASAGER FOR PASAGER@link_bd_am;
+
+
+-- D.Transparența actualizării (Triggere INSTEAD OF) 
+-- Triggeri pentru a permite update pe vederi
+-- Update pe rezervare: trimitem la AM daca id-ul e impar, altfel la EU
+CREATE OR REPLACE TRIGGER trg_upd_rezervare_global
+INSTEAD OF UPDATE ON V_REZERVARE
+FOR EACH ROW
+BEGIN
+    IF MOD(:OLD.id_rezervare, 2) != 0 THEN
+        UPDATE REZERVARE_AM@link_bd_am
+        SET status = :NEW.status, total_de_plata = :NEW.total_de_plata
+        WHERE id_rezervare = :OLD.id_rezervare;
+    ELSE
+        UPDATE REZERVARE_EU@link_bd_eu
+        SET status = :NEW.status, total_de_plata = :NEW.total_de_plata
+        WHERE id_rezervare = :OLD.id_rezervare;
+    END IF;
+END;
+/
+
+-- Update pe datele utilizatorului (trimitem pe AM)
+CREATE OR REPLACE TRIGGER trg_upd_utilizator_global
+INSTEAD OF UPDATE ON V_UTILIZATOR
+FOR EACH ROW
+BEGIN
+    UPDATE UTILIZATOR_DATA@link_bd_am
+    SET nume    = :NEW.nume,
+        prenume = :NEW.prenume,
+        telefon = :NEW.telefon
+    WHERE id_user = :OLD.id_user;
+END;
+/
